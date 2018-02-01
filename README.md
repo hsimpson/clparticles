d1a802c2f1162fc6d8837d9030c2a27010f8b64c
Daniel Toplak <homer.simpson123@gmx.de>
2018-02-01 02:30:50 +0100

    Initial setup with gl3w and glfw

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-clparticles
+# CLParticles
+
+Calculating particles with OpenCL and rendering with OpenGL
+
+## Dependencies
+
+- gl3w
+- glfw
+
+## Building
+
+### Prerequisites
+
+- Visual Studio 2017
+- Python
+- CMake
+
+### Build gl3w
+
+```sh
+cd gl3w
+python gl3w_gen.py
+```
+
+### Build glfw
+
+```sh
+cd glfw
+mkdir glfw-build
+cd glfw-build
+cmake .. -DBUILD_SHARED_LIBS=ON -DUSE_MSVC_RUNTIME_LIBRARY_DLL=ON -G "Visual Studio 15 2017 Win64"
+```
+
+Open ```glfw-build\GLFW.sln``` in Visual Studio 2017 and build both Debug and Release
---
+++ src/clparticles.cpp
@@ -0,0 +1,63 @@
+// clparticles.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <GL/gl3w.h>    // This example is using gl3w to access OpenGL functions (because it is small). You may use glew/glad/glLoadGen/etc. whatever already works for you.
+#include <GLFW/glfw3.h>
+
+static void error_callback(int error, const char* description)
+{
+	fprintf(stderr, "Error %d: %s\n", error, description);
+}
+
+
+int main()
+{
+	GLFWwindow* window;
+
+	// Setup window
+	glfwSetErrorCallback(error_callback);
+	if (!glfwInit())
+		return 1;
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(1280, 720, "CLParticles", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	glfwSwapInterval(1); // Enable vsync
+	gl3wInit();
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Poll for and process events */
+		glfwPollEvents();
+
+		/* Render here */
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
